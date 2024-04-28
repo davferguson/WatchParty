4965fce843efff464e204d45a648394fef61debb
Davin Ferguson <davferg43@gmail.com>
2024-04-28 19:11:30 -0400

    video state is now controlled through database and is updated automatically

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,26 +4,51 @@ import YouTube from 'react-youtube'
 import YoutubeService from '../services/YoutubeService'
 import { db } from '../firebase.js'
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/user/userSlice.js'
 // import { collection, query, doc, onSnapshot } from "firebase/firestore";
 
 function Chat() {
+  const user = useSelector(selectUser);
+  const groupDbRef = doc(db, "groups", "dareds-group");
+
   const [videos, setVideos] = useState([]);
   const [videoId, setVideoId] = useState("");
-  const [curVideoId, setCurVideoId] = useState("");
+  const [groupData, setGroupData] = useState({
+    curVideoId: "",
+    isPlaying: false,
+    members: [],
+    userReady: []
+  });
   const [player, setPlayer] = useState(null);
 
   const sendVideo = async (e) => {
     e.preventDefault();
     
-    const groupDbRef = doc(db, "groups", "dareds-group");
     await updateDoc(groupDbRef, {
       "curVideoId": videoId
     });
-  }
+  };
 
-  const onPlayerReady = (e) => {
+  const onPlayerReady = async (e) => {
     setPlayer(e.target);
-  }
+    const field = "userReady." + user.displayName;
+    await updateDoc(groupDbRef, {
+      [field]: true,
+    });
+  };
+
+  const onPlayHandler = async () => {
+    await updateDoc(groupDbRef, {
+      "isPlaying": true
+    });
+  };
+
+  const onPauseHandler = async () => {
+    await updateDoc(groupDbRef, {
+      "isPlaying": false
+    });
+  };
 
 //   const q = query(collection(db, "groups/dareds-group/posts"));
 //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -36,13 +61,20 @@ function Chat() {
 
   useEffect(() => {
     const unsub = onSnapshot(doc(db, "groups", "dareds-group"), (doc) => {
-      console.log("Current data: ", doc.data());
-      setCurVideoId(doc.data().curVideoId)
+      console.log("Data Changed:", doc.data());
+      setGroupData(doc.data());
     });
     return () => {
+      onPauseHandler();
       unsub();
     }
   }, []);
+
+  useEffect(() => {
+    if(player) {
+      groupData.isPlaying ? player.playVideo() : player.pauseVideo();
+    }
+  }, [groupData.isPlaying]);
 
   async function findVideos() {
     try{
@@ -62,12 +94,18 @@ function Chat() {
   const opts = {
     height: '315',
     width: '560',
-  }
+    playerVars: {
+      controls: 0,
+    }
+  };
+
   return (
     <div className="chat">
         <h4>Chat</h4>
         {/* <button onClick={findVideos}>Do The Search</button> */}
-        { curVideoId && <YouTube videoId={curVideoId} onReady={onPlayerReady} opts={opts} /> }
+        { groupData.curVideoId && <YouTube videoId={groupData.curVideoId} onReady={onPlayerReady} opts={opts} />}
+        { groupData.curVideoId && <button onClick={onPlayHandler}>Play</button>}
+        { groupData.curVideoId && <button onClick={onPauseHandler}>Pause</button>}
         <form>
             <input value={videoId} onChange={e => setVideoId(e.target.value)} placeholder='Video Id' type='text' />
             <button type='submit' onClick={sendVideo}>Send Video</button>

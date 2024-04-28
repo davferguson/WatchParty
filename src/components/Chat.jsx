b3b3f58e8fe67cd855910148ea22d4c7ab732ba6
Davin Ferguson <davferg43@gmail.com>
2024-04-28 15:42:13 -0400

    changed persistance of firebase auth. send video now updates database

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,23 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Chat.css'
 import YouTube from 'react-youtube'
 import YoutubeService from '../services/YoutubeService'
-// import { db } from '../firebase.js'
+import { db } from '../firebase.js'
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 // import { collection, query, doc, onSnapshot } from "firebase/firestore";
 
 function Chat() {
   const [videos, setVideos] = useState([]);
   const [videoId, setVideoId] = useState("");
   const [curVideoId, setCurVideoId] = useState("");
+  const [player, setPlayer] = useState(null);
 
-  const loadVideo = (e) => {
+  const sendVideo = async (e) => {
     e.preventDefault();
-    setCurVideoId(videoId);
+    
+    const groupDbRef = doc(db, "groups", "dareds-group");
+    await updateDoc(groupDbRef, {
+      "curVideoId": videoId
+    });
   }
 
-//   const unsub = onSnapshot(doc(db, "groups", "dareds-group"), (doc) => {
-//     console.log("Current data: ", doc.data());
-//   });
+  const onPlayerReady = (e) => {
+    setPlayer(e.target);
+  }
 
 //   const q = query(collection(db, "groups/dareds-group/posts"));
 //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -27,6 +33,16 @@ function Chat() {
 //     });
 //     console.log("Messages: ", posts.join(", "));
 //   });
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "groups", "dareds-group"), (doc) => {
+      console.log("Current data: ", doc.data());
+      setCurVideoId(doc.data().curVideoId)
+    });
+    return () => {
+      unsub();
+    }
+  }, []);
 
   async function findVideos() {
     try{
@@ -51,10 +67,10 @@ function Chat() {
     <div className="chat">
         <h4>Chat</h4>
         {/* <button onClick={findVideos}>Do The Search</button> */}
-        { curVideoId && <YouTube videoId={curVideoId} opts={opts} /> }
+        { curVideoId && <YouTube videoId={curVideoId} onReady={onPlayerReady} opts={opts} /> }
         <form>
             <input value={videoId} onChange={e => setVideoId(e.target.value)} placeholder='Video Id' type='text' />
-            <button type='submit' onClick={loadVideo}>Load Video</button>
+            <button type='submit' onClick={sendVideo}>Send Video</button>
         </form>
 
         {videos.map(({ id, title }) => (
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,6 +16,9 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+// Auth state will only persist in current session/tab
+setPersistence(auth, browserSessionPersistence);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
